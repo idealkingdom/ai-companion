3970aaa4348b2659101d682dd6a7fe1d3051cd09
idealkingdom <jefferic506@gmail.com>
2025-11-15 01:04:28 +0800

    UI enhancements

--- webview/chatbox/chatbox.js
+++ webview/chatbox/chatbox.js
@@ -142,7 +142,7 @@ function showLoadingIndicator() {
     
     loadingDiv.innerHTML = `
         <div class="message-content">
-            <span class="ai-icon">🤖</span>
+            <span class="ai-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bot-message-square-icon lucide-bot-message-square"><path d="M12 6V2H8"/><path d="M15 11v2"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="M20 16a2 2 0 0 1-2 2H8.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 4 20.286V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2z"/><path d="M9 11v2"/></svg></span>
             <div class="loading-dots">
                 <span></span>
                 <span></span>
@@ -197,7 +197,7 @@ function appendAIMessage(response) {
   const parsedResponse = marked.parse(response);
   const systemResponseHTML = `<div class="system-message">
             <div class="message-content">
-                <div class="message-header"><span class="ai-icon">🤖</span> Companion</div>
+                <div class="message-header"><span class="ai-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bot-message-square-icon lucide-bot-message-square"><path d="M12 6V2H8"/><path d="M15 11v2"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="M20 16a2 2 0 0 1-2 2H8.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 4 20.286V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2z"/><path d="M9 11v2"/></svg></span> Companion</div>
                 <span class="message-text">${parsedResponse}</span>
                 <div class="message-time">${getCurrentDate()}</div>
             </div>
@@ -223,11 +223,11 @@ function chatRequest(content){
 
 function resetChat(content) {
     chatMessages.innerHTML = '';
-    welComeMessage.classList.remove('hidden'); 
+    console.error(content.uid);
     chatLog.dataset.chatId = content.uid;
     attachedImages = [];
     renderAttachments();
-    
+    chatWelcomeMessage.classList.remove('hidden');
     chatMessage.focus();
 }
 
